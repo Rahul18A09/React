18e3f81e2438d3ab104954e04e9a6e1de1f7086a
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-24 22:29:38 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var a = 10;
 
-const arr = [10,20,30,40,50]
+export const arr = [10,20,30,40,50]
 
 export default arr
--- script.js
+++ script.js
@@ -4,6 +4,6 @@
 
 // document.body.appendChild(h1);
 
-import a from "./app.js"
-console.log(a);
+import arr from "./app.js"
+console.log(arr);
 
