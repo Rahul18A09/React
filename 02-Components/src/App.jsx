b30d8a9b26be632b55bdbbd25cbd1f15535d2eb0
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-27 00:10:51 +0530

    update

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Card from "./components/Card"
+import Card from "./components/Card";
 
 const App = () => {
+  const user = "Bharada";
+  const age = 25;
   return (
     <div>
-      <div className="card">
-        <h1>Rahul Bharada</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-          dignissimos.
-        </p>
-      </div>
-
-      {Card()}
+      <Card />
     </div>
   );
 };
--- 02-Components/src/components/Card.jsx
+++ 02-Components/src/components/Card.jsx
@@ -1,5 +1,14 @@
-function Card(){
-    return "Hi I am Card"
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Rahul Bharada</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, dignissimos.</p>
+      </div>
+    </div>
+  )
 }
 
-export default Card;
+export default Card
