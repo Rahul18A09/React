9c40f4e8ef3ba300840143b0a3019bd087880c2f
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-30 09:12:10 +0530

    new

---
+++ 03-Props/03-Props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './Components/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1760008486599-e62d30e2ca95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4NHxDRHd1d1hKQWJFd3x8ZW58MHx8fHx8" alt="" />
+        <h1>Rahul Bharada</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
