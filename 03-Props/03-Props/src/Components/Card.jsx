4f2d0a22b22f118cf1568a070fa91137acc1f51b
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-30 13:42:28 +0530

    update

--- 03-Props/03-Props/src/Components/Card.jsx
+++ 03-Props/03-Props/src/Components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
   return (
     <div>
       <div className="card">
-        <img src="https://images.unsplash.com/photo-1760008486599-e62d30e2ca95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4NHxDRHd1d1hKQWJFd3x8ZW58MHx8fHx8" alt="" />
+        <img src={props.img} alt="" />
         <h1>{props.user},{props.age}</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
         <button>View Profile</button>
