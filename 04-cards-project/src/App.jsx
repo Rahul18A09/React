8e55a869c38ea355e9dc4fa2aafa4a4f7a0d3952
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-07 10:55:11 +0530

    new update

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Bookmark } from "lucide-react";
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png"
+              alt="logo"
+            />
+            <button>
+              Save <Bookmark size={12} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h2>
+              Amazon <span>% days ago</span>
+            </h2>
+            <h3>Senior UI/UX Designer</h3>
+
+            <div className="tag">
+              <h4>Part Time</h4>
+              <h4>Senior level</h4>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png"
+              alt="logo"
+            />
+            <button>
+              Save <Bookmark size={12} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h2>
+              Amazon <span>% days ago</span>
+            </h2>
+            <h3>Senior UI/UX Designer</h3>
+
+            <div className="tag">
+              <h4>Part Time</h4>
+              <h4>Senior level</h4>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
