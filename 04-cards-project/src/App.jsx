811ecab9d612a2fc7664472ca8da7ede8d95a096
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-07 17:23:47 +0530

    new

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Card from "./components/Card";
-import User from "./components/User";
+
+
 
 const App = () => {
   return (
     <div className="parent">
- 
-      <Card/>
-      <Card/>
-      <Card/>
-    
+      <Card />
 
     </div>
   );
