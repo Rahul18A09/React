bdd74a192134091fa59a13f1d42e8a8a16aac22d
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-07 09:11:30 +0530

    update

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -3,14 +3,23 @@ import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const user = "Rahul";
-  const age = 25;
   return (
-    <div>
-      <Navbar/>
-      <Navbar/>
-      <Card/>
-      <Card/>
+    <div className="parent">
+      <div>
+        <Card
+          user="Rahul"
+          age="25"
+          img="https://images.unsplash.com/photo-1764968351728-4b2938f56873?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMnx8fGVufDB8fHx8fA%3D%3D"
+        />
+      </div>
+
+      <div>
+        <Card
+          user="Rahul"
+          age="25"
+          img="https://images.unsplash.com/photo-1764968351728-4b2938f56873?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMnx8fGVufDB8fHx8fA%3D%3D"
+        />
+      </div>
     </div>
   );
 };
--- 02-Components/src/components/Card.jsx
+++ 02-Components/src/components/Card.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props);
+
   return (
     <div>
-    <div className="card">
-        <h1>Rahul Bharada</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dicta.</p>
-        <p>and  I am 25 year old.</p>
+      <div className="card">
+        <img src={props.img} alt="img" />
+        <h1>{props.user}</h1>
+        <h2>I am {props.age} year old.</h2>
+        <button>View More</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
-
-
-
