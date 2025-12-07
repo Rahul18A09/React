b881b73e61f4e07e12735a86e7225e28e75b0703
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-07 08:17:57 +0530

    delete

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  const user = "Bharada";
+  const user = "Rahul";
   const age = 25;
   return (
     <div>
-      <Card />
+      <Navbar/>
+      <Navbar/>
+      <Card/>
+      <Card/>
     </div>
   );
 };
--- 02-Components/src/components/Card.jsx
+++ 02-Components/src/components/Card.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 const Card = () => {
   return (
     <div>
-      <div className='card'>
+    <div className="card">
         <h1>Rahul Bharada</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, dignissimos.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dicta.</p>
+        <p>and  I am 25 year old.</p>
       </div>
     </div>
   )
 }
 
-export default Card
+export default Card;
+
+
+
