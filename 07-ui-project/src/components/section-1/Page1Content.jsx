43901aa49efe1c7460341286a473a05ae8a8fe2f
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-14 12:59:06 +0530

    new-update

--- 07-ui-project/src/components/section-1/Page1Content.jsx
+++ 07-ui-project/src/components/section-1/Page1Content.jsx
@@ -5,7 +5,7 @@ import RightContent from "./RightContent";
 
 const Page1Content = (props) => {
   return (
-    <div className="py-10 flex items-center gap-10 h-[90vh]">
+    <div className="pb-16 pt-6 flex items-center gap-10 h-[90vh] px-18">
      <LeftCotent/>
      <RightContent users={props.users}/>
     </div>
--- 07-ui-project/src/components/section-1/RightCard.jsx
+++ 07-ui-project/src/components/section-1/RightCard.jsx
@@ -5,9 +5,9 @@ const RightCard = (props) => {
     console.log(props.color);
     
   return (
-    <div>
+    <div className='h-full shrink-0 overflow-hidden relative w-80 rounded-4xl'>
       <img className='h-full w-full object-cover' src={props.img} alt="" />
-      <RightCardContent/>
+      <RightCardContent color={props.color} id={props.id} tag={props.tag}/>
     </div>
   )
 }
