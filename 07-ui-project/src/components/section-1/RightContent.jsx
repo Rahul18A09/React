322cf93a8e2efb92af135ef8b7a2038124e9e6b0
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-14 09:00:38 +0530

    update

--- 07-ui-project/src/components/section-1/RightContent.jsx
+++ 07-ui-project/src/components/section-1/RightContent.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
 
 const RightContent = () => {
   return (
-    <div className='h-full w-3/4 bg-blue-600'>
-     <div></div>
-     <div></div>
+    <div className='h-full w-3/4 bg-blue-200'>
+   
     </div>
   )
 }
