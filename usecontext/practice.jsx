2641d7bc2760f4a2003125585a44cd1e534eb7fc
Rahul Bharada <147308572+Rahul18A09@users.noreply.github.com>
2026-01-22 22:25:29 +0530

    Update practice.jsx
    Update

--- usecontext/practice.jsx
+++ usecontext/practice.jsx
@@ -3,3 +3,9 @@ what is useContext?
 //useContext is a React Hook that helps you share data globally between components without passing props manually at every level.
 
 useContext allows components to directly access shared data from a central place
+
+Use it when data is needed in many components, such as:
+User login info
+Theme (dark/light mode)
+Language
+Global settings
