f515ae5ab3caba99d2cc0c0039ebcd3d4970e4bb
Rahul Bharada <147308572+Rahul18A09@users.noreply.github.com>
2026-01-22 22:05:07 +0530

    Create practice.jsx
    new

---
+++ usecontext/practice.jsx
@@ -0,0 +1,5 @@
+what is useContext?
+
+//useContext is a React Hook that helps you share data globally between components without passing props manually at every level.
+
+useContext allows components to directly access shared data from a central place
