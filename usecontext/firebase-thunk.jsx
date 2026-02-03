983d898be60eaedbdc6bf9a19990deda6fcc7589
Rahul Bharada <147308572+Rahul18A09@users.noreply.github.com>
2026-02-03 22:15:31 +0530

    Update firebase-thunk.jsx
    update

--- usecontext/firebase-thunk.jsx
+++ usecontext/firebase-thunk.jsx
@@ -6,3 +6,7 @@ rejected
 It automatically dispatches lifecycle actions.
 Firebase async calls happen inside thunk.
 Redux updates state when promise resolves.
+
+How To Explain In Interview
+You can say:
+"I used Redux Toolkit with createAsyncThunk to handle asynchronous Firebase operations like fetching and adding data. Thunk allowed me to separate async logic from components and manage loading states cleanly inside extraReducers."
